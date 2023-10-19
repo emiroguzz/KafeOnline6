ecaa7f309d50731cf9e3270afc6215659d337487
EmirOguz <eemiroguzz@hotmail.com>
2023-10-19 17:21:53 +0300

    Düzeltmeler yapıldı.

--- AnaForm.Designer.cs
+++ AnaForm.Designer.cs
@@ -28,32 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1=new Label();
+            menuStrip1=new MenuStrip();
+            tsmiUrunler=new ToolStripMenuItem();
+            tsmiGecmisSiparisler=new ToolStripMenuItem();
+            lvwUrunler=new ListView();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // label1
+            // menuStrip1
             // 
-            label1.AutoSize=true;
-            label1.Location=new Point(12, 9);
-            label1.Name="label1";
-            label1.Size=new Size(38, 15);
-            label1.TabIndex=0;
-            label1.Text="label1";
+            menuStrip1.Items.AddRange(new ToolStripItem[] { tsmiUrunler, tsmiGecmisSiparisler });
+            menuStrip1.Location=new Point(0, 0);
+            menuStrip1.Name="menuStrip1";
+            menuStrip1.Size=new Size(614, 29);
+            menuStrip1.TabIndex=0;
+            menuStrip1.Text="menuStrip1";
             // 
-            // Form1
+            // tsmiUrunler
+            // 
+            tsmiUrunler.Font=new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            tsmiUrunler.Name="tsmiUrunler";
+            tsmiUrunler.Size=new Size(75, 25);
+            tsmiUrunler.Text="Ürünler";
+            // 
+            // tsmiGecmisSiparisler
+            // 
+            tsmiGecmisSiparisler.Font=new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            tsmiGecmisSiparisler.Name="tsmiGecmisSiparisler";
+            tsmiGecmisSiparisler.Size=new Size(142, 25);
+            tsmiGecmisSiparisler.Text="Geçmiş Siparişler";
+            // 
+            // lvwUrunler
+            // 
+            lvwUrunler.Dock=DockStyle.Fill;
+            lvwUrunler.Location=new Point(0, 29);
+            lvwUrunler.Name="lvwUrunler";
+            lvwUrunler.Size=new Size(614, 421);
+            lvwUrunler.TabIndex=1;
+            lvwUrunler.UseCompatibleStateImageBehavior=false;
+            // 
+            // AnaForm
             // 
             AutoScaleDimensions=new SizeF(7F, 15F);
             AutoScaleMode=AutoScaleMode.Font;
-            ClientSize=new Size(800, 450);
-            Controls.Add(label1);
-            Name="Form1";
-            Text="Form1";
+            ClientSize=new Size(614, 450);
+            Controls.Add(lvwUrunler);
+            Controls.Add(menuStrip1);
+            MainMenuStrip=menuStrip1;
+            Name="AnaForm";
+            StartPosition=FormStartPosition.CenterParent;
+            Text="Kafe Online6";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem tsmiUrunler;
+        private ToolStripMenuItem tsmiGecmisSiparisler;
+        private ListView lvwUrunler;
     }
 }
